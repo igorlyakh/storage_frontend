8b7d582ec9f90df448208e922db385ce264dc077
Igor Liakh <igorlyah060@gmail.com>
2026-02-14 16:26:44 +0200

    added isGlobalLoader selector

--- src/store/selectors/selectors.js
+++ src/store/selectors/selectors.js
@@ -1,2 +1,14 @@
 export const usernameSelector = state => state.user.username;
 export const tokenSelector = state => state.user.accessToken;
+
+export const isGlobalLoading = state => {
+  const hasActiveMutation = Object.values(state.api.mutations).some(
+    mutation => mutation?.status === 'pending',
+  );
+
+  const hasActiveQueries = Object.values(state.api.queries).some(
+    query => query?.status === 'pending',
+  );
+
+  return hasActiveMutation || hasActiveQueries;
+};
