2b046b79419975defe80190282163e05d6ef4e39
Igor Liakh <igorlyah060@gmail.com>
2026-02-14 16:33:22 +0200

    added global loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
+import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
+import Loader from './components/Loader';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RestrictedRouts from './pages/RestrictedRoutes';
+import { isGlobalLoading } from './store/selectors/selectors';
 
 const App = () => {
+  const isLoading = useSelector(isGlobalLoading);
+
   return (
-    <Routes>
-      <Route
-        element={<Layout />}
-        path="/"
-      >
+    <>
+      {isLoading && <Loader />}
+      <Routes>
         <Route
-          element={<div>Home Page</div>}
-          index
-        />
-        <Route
-          path="login"
-          element={
-            <RestrictedRouts redirectTo="/">
-              <LoginPage />
-            </RestrictedRouts>
-          }
-        />
-      </Route>
-    </Routes>
+          element={<Layout />}
+          path="/"
+        >
+          <Route
+            element={<div>Home Page</div>}
+            index
+          />
+          <Route
+            path="login"
+            element={
+              <RestrictedRouts redirectTo="/">
+                <LoginPage />
+              </RestrictedRouts>
+            }
+          />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
