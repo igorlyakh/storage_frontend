f209b1b09e8bbc000c2ec8453a7c06c5047e2273
Igor Liakh <igorlyah060@gmail.com>
2026-02-14 16:50:44 +0200

    fix loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,19 @@
 import { ColorRing } from 'react-loader-spinner';
+import styles from './styles.module.scss';
 
 const Loader = () => {
   return (
-    <ColorRing
-      visible={true}
-      height="100"
-      width="100"
-      ariaLabel="color-ring-loading"
-      wrapperStyle={{ position: 'absolute', top: '50%', left: '50%' }}
-      wrapperClass="color-ring-wrapper"
-      colors={['#849b87', '#849b87', '#849b87', '#849b87', '#849b87']}
-    />
+    <div className={styles.wrapper}>
+      <ColorRing
+        visible={true}
+        height="100"
+        width="100"
+        ariaLabel="color-ring-loading"
+        wrapperStyle={{}}
+        wrapperClass="color-ring-wrapper"
+        colors={['#849b87', '#849b87', '#849b87', '#849b87', '#849b87']}
+      />
+    </div>
   );
 };
 
