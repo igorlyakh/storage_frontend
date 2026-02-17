2d4358194bab36d43321757752136fb509edf341
Igor Liakh <igorlyah060@gmail.com>
2026-02-17 19:04:02 +0200

    added orderItem

---
+++ src/components/OrderItem/OrderItem.jsx
@@ -0,0 +1,32 @@
+import dayjs from 'dayjs';
+import { ArrowRight } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import styles from './styles.module.scss';
+
+const OrderItem = ({ store, status, sended, updated }) => {
+  return (
+    <div className={styles.item}>
+      <p>
+        <span>From: </span>
+        <span>{store}</span>
+      </p>
+      <p>
+        <span>Status: </span>
+        <span>{status}</span>
+      </p>
+      <p>
+        <span>Sended: </span>
+        <span>{dayjs(sended).format('DD.MM.YYYY HH:mm:ss')}</span>
+      </p>
+      <p>
+        <span>Updated: </span>
+        <span>{dayjs(updated).format('DD.MM.YYYY HH:mm:ss')}</span>
+      </p>
+      <Link>
+        To order <ArrowRight />
+      </Link>
+    </div>
+  );
+};
+
+export default OrderItem;
--- src/components/OrdersList/OrdersList.jsx
+++ src/components/OrdersList/OrdersList.jsx
@@ -1,47 +1,17 @@
-import dayjs from 'dayjs';
-import { ArrowRight } from 'lucide-react';
-import { Link } from 'react-router-dom';
+import OrderItem from '../OrderItem/OrderItem';
 import styles from './styles.module.scss';
 
 const OrdersList = ({ data }) => {
   return (
     <ul className={styles.list}>
       {data?.map(order => (
-        <li key={order.id}>
-          <div>
-            <span>Status:</span> <span>{order.status}</span>
-          </div>
-          <div>
-            <span>Form store:</span> <span>{order.store.name}</span>
-          </div>
-          <span>items:</span>
-          <ul>
-            {order.items.map(item => (
-              <li key={item.id}>
-                <div>
-                  <span>Product name: </span> <span>{item.product.name}</span>
-                </div>
-                <div>
-                  <span>Qty: </span> <span>{item.requestedQty}</span>
-                </div>
-                <div>
-                  <span>Category:</span> <span>{item.product.category}</span>
-                </div>
-              </li>
-            ))}
-          </ul>
-          <div>
-            <span>Sended:</span>
-            <span>{dayjs(order.createdAt).format('DD.MM.YYYY HH:mm:ss ')}</span>
-          </div>
-          <div>
-            <span>Changed:</span>{' '}
-            <span>{dayjs(order.updatedAt).format('DD.MM.YYYY HH:mm:ss')}</span>
-          </div>
-          <Link>
-            To order <ArrowRight />
-          </Link>
-        </li>
+        <OrderItem
+          key={order.id}
+          store={order.store.name}
+          status={order.status}
+          sended={order.createdAt}
+          updated={order.updatedAt}
+        />
       ))}
     </ul>
   );
