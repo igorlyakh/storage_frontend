04880b8ba08e0fa8caef628b0679e7984265ab5c
Igor Liakh <igorlyah060@gmail.com>
2026-02-21 13:07:04 +0200

    updated lib for tables

--- src/components/ProductsTable/ProductsTable.jsx
+++ src/components/ProductsTable/ProductsTable.jsx
@@ -1,7 +1,9 @@
 import { useGetAllProductsQuery } from '../../store/api/api';
-
 const ProductsTable = () => {
-  const { data } = useGetAllProductsQuery();
+  const { data, isLoading } = useGetAllProductsQuery();
+  if (isLoading) {
+    return null;
+  }
   return <div>{data[1]?.name}</div>;
 };
 
