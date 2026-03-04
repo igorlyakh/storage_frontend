5a155bc58f8e1cfcbdee672a1f86fb20ba254904
Igor Liakh <igorlyah060@gmail.com>
2026-03-04 22:07:59 +0200

    updated link to order page

--- src/components/OrderItem/OrderItem.jsx
+++ src/components/OrderItem/OrderItem.jsx
@@ -37,7 +37,7 @@ const OrderItem = ({ store, status, sended, updated, id }) => {
       )}
       <Link
         className={styles.link}
-        to={`${id}`}
+        to={`/orders/${id}`}
       >
         <span>To order</span> <ArrowRight />
       </Link>
