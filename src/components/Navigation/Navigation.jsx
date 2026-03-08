2cab3aadf3de1281ce673a3d6a4f612894bcb8a1
Igor Liakh <igorlyah060@gmail.com>
2026-03-08 19:46:33 +0200

    refactoring in header

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,100 @@
+import toast from 'react-hot-toast';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useLogoutMutation } from '../../store/api/api';
+import { tokenSelector, userRoleSelector } from '../../store/selectors/selectors';
+import { logoutAction } from '../../store/userSlice/userSlice';
+import DropdownMenu from '../DropDownMenu/DropDownMenu';
+
+import Button from '../Button';
+import styles from './styles.module.scss';
+
+const Navigation = () => {
+  const token = useSelector(tokenSelector);
+  const role = useSelector(userRoleSelector);
+  const dispatch = useDispatch();
+  const [logout] = useLogoutMutation();
+  const handler = async () => {
+    try {
+      await logout().unwrap();
+      dispatch(logoutAction());
+      toast.success('You have successfully logged out!');
+    } catch {
+      toast.error('Something went wrong! Try again.');
+    }
+  };
+
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.menu}>
+        <li>
+          <Link
+            className={styles.link}
+            to="/"
+          >
+            Home
+          </Link>
+        </li>
+        {['ADMIN', 'WAREHOUSE'].includes(role) && (
+          <li>
+            <Link
+              className={styles.link}
+              to="/all-orders"
+            >
+              All orders
+            </Link>
+          </li>
+        )}
+        {role === 'STORE' && (
+          <li>
+            <Link
+              className={styles.link}
+              to="/orders"
+            >
+              Orders
+            </Link>
+          </li>
+        )}
+        {role === 'ADMIN' && (
+          <li>
+            <DropdownMenu
+              title="Products"
+              items={[
+                { label: 'All Products', to: '/products' },
+                { label: 'Create Product', to: '/products/create' },
+              ]}
+            />
+          </li>
+        )}
+        {role === 'ADMIN' && (
+          <li>
+            <DropdownMenu
+              title="Users"
+              items={[
+                { label: 'All Users', to: '/users' },
+                { label: 'Create User', to: '/users/create' },
+              ]}
+            />
+          </li>
+        )}
+        <li>
+          {token ? (
+            <Button
+              text={'Logout'}
+              onClick={handler}
+            />
+          ) : (
+            <Link
+              className={styles.link}
+              to="/login"
+            >
+              Login
+            </Link>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
