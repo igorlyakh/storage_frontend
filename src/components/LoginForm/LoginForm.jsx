1d5fa6ff44f3180039230e7a78a17a0327232ad4
Igor Liakh <igorlyah060@gmail.com>
2026-04-18 14:49:26 +0300

    updated login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,13 +1,28 @@
+import {
+  Button,
+  Center,
+  Paper,
+  PasswordInput,
+  Stack,
+  TextInput,
+  Title,
+} from '@mantine/core';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import { useDispatch } from 'react-redux';
 import { useLoginMutation } from '../../store/api/api';
 import { setData } from '../../store/userSlice/userSlice';
-import styles from './styles.module.scss';
 
 const LoginForm = () => {
-  const [login] = useLoginMutation();
-  const { register, handleSubmit, reset } = useForm();
+  const [login, { isLoading }] = useLoginMutation();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
   const dispatch = useDispatch();
 
   const handler = async formData => {
@@ -15,55 +30,70 @@ const LoginForm = () => {
       const data = await login(formData).unwrap();
       dispatch(setData(data));
       toast.success(`Welcome, ${data.username}!`);
-    } catch (error) {
-      toast.error(error.data?.message || error.message);
-    } finally {
       reset();
+    } catch (error) {
+      toast.error(error.data?.message || error.message || 'Login failed');
     }
   };
 
   return (
-    <div className={styles.form__wrapper}>
-      <form
-        onSubmit={handleSubmit(data => {
-          handler(data);
-        })}
-        className={styles.form}
+    <Center
+      h="100%"
+      style={{ marginTop: 150 }}
+    >
+      <Paper
+        withBorder
+        shadow="md"
+        p={30}
+        radius="md"
+        w={{ base: 300, sm: 400 }}
       >
-        <label className={styles.wrapper}>
-          <span>Username:</span>
-          <input
-            type="text"
-            placeholder="Enter your username"
-            className={styles.input}
-            {...register('username', {
-              required: 'Username is required!',
-            })}
-          />
-        </label>
-        <label className={styles.wrapper}>
-          <span>Password:</span>
-          <input
-            type="password"
-            placeholder="Enter your password"
-            className={styles.input}
-            {...register('password', {
-              required: 'Password is required!',
-              minLength: {
-                value: 6,
-                message: 'Password must be longer than 6 characters!',
-              },
-            })}
-          />
-        </label>
-        <button
-          className={styles.btn}
-          type="submit"
+        <Title
+          order={2}
+          ta="center"
+          mb="lg"
         >
-          Login
-        </button>
-      </form>
-    </div>
+          Welcome to Stock Control
+        </Title>
+
+        <form onSubmit={handleSubmit(handler)}>
+          <Stack>
+            <TextInput
+              label="Username"
+              placeholder="Enter your username"
+              withAsterisk
+              {...register('username', {
+                required: 'Username is required!',
+              })}
+              error={errors.username?.message}
+            />
+
+            <PasswordInput
+              label="Password"
+              placeholder="Enter your password"
+              withAsterisk
+              {...register('password', {
+                required: 'Password is required!',
+                minLength: {
+                  value: 6,
+                  message: 'Password must be longer than 6 characters!',
+                },
+              })}
+              error={errors.password?.message}
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              mt="md"
+              loading={isLoading}
+            >
+              Login
+            </Button>
+          </Stack>
+        </form>
+      </Paper>
+    </Center>
   );
 };
 
