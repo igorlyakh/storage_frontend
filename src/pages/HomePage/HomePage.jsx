6734ba27aa0f531f50724926e8a9c59bc6e9263c
Igor Liakh <igorlyah060@gmail.com>
2026-05-11 20:29:05 +0300

    added important window on home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,21 @@
-import { Button, Container, Group, Paper, Stack, Text, Title } from '@mantine/core';
+import {
+  Button,
+  Container,
+  Group,
+  Paper,
+  Stack,
+  Text,
+  ThemeIcon,
+  Title,
+} from '@mantine/core';
+import { Info } from 'lucide-react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { userRoleSelector } from '../../store/selectors/selectors';
 
 const HomePage = () => {
   const userRole = useSelector(userRoleSelector);
+
   return (
     <Container
       size="md"
@@ -47,26 +58,75 @@ const HomePage = () => {
           and to provide a simplified, effortless ordering experience for retail stores.
         </Text>
 
-        <Text
-          c="dimmed"
-          size="md"
-          ta="center"
-          maw={700}
-          visibleFrom="sm"
+        <Paper
+          withBorder
+          p="md"
+          radius="md"
+          shadow="xs"
+          bg="blue.0"
+          style={{ borderColor: 'var(--mantine-color-blue-4)' }}
+          w="100%"
+          maw={500}
         >
-          Streamline your entire supply chain, track product availability in real-time,
-          and manage automated replenishment requests with absolute precision. Built for
-          speed, accuracy, and uninterrupted collaboration across your entire retail
-          network.
-        </Text>
+          <Stack
+            gap="xs"
+            align="center"
+          >
+            <Group gap="xs">
+              <ThemeIcon
+                variant="light"
+                color="blue"
+                size="sm"
+                radius="xl"
+              >
+                <Info size={14} />
+              </ThemeIcon>
+              <Text
+                fw={700}
+                c="blue.9"
+                fz="xs"
+                tt="uppercase"
+              >
+                Important Information
+              </Text>
+            </Group>
+
+            <Text
+              ta="center"
+              fw={800}
+              fz="md"
+              c="blue.9"
+              style={{ lineHeight: 1.3 }}
+            >
+              Orders are accepted from Sunday to Thursday until{' '}
+              <Text
+                component="span"
+                inherit
+                c="orange.5"
+              >
+                14:30
+              </Text>
+            </Text>
+
+            <Text
+              ta="center"
+              size="xs"
+              c="blue.8"
+              style={{ lineHeight: 1.4 }}
+            >
+              All orders received <b>after 14:30</b> will be processed and shipped the
+              following day. Orders placed on <b>Friday and Saturday</b> will be processed
+              and shipped on <b>Sunday</b>.
+            </Text>
+          </Stack>
+        </Paper>
 
         <Paper
           withBorder
-          p={{ base: 'md', sm: 'lg' }}
+          p="md"
           radius="md"
           shadow="sm"
           bg="gray.0"
-          mt="xl"
           w="100%"
           maw={500}
         >
@@ -76,14 +136,14 @@ const HomePage = () => {
           >
             <Text
               fw={600}
-              size="lg"
+              size="md"
               c="dark.8"
             >
               Need assistance?
             </Text>
             <Text
               ta="center"
-              size="sm"
+              size="xs"
               c="gray.6"
             >
               If you experience any issues, have feature requests, or need technical
@@ -92,8 +152,7 @@ const HomePage = () => {
             <Text
               ta="center"
               fw={500}
-              mt="sm"
-              fz="sm"
+              fz="xs"
             >
               Please contact us at:{' '}
               <Text
@@ -110,7 +169,7 @@ const HomePage = () => {
         </Paper>
 
         <Group
-          mt="xl"
+          mt="lg"
           w={{ base: '100%', sm: 'auto' }}
         >
           <Button
